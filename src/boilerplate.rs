ae28a197ab0fc02e1c5e1fd3eb424139a859698e
Dima Lazarev <dmitry.lazarev@gmail.com>
2025-12-02 07:27:13 +0200

    Update boilerplate

--- src/boilerplate.rs
+++ src/boilerplate.rs
@@ -3,7 +3,10 @@ use aoc_runner_derive::{aoc, aoc_generator};
 #[aoc_generator(day1)]
 fn parse_input(input: &str) -> () {
     use aoc_parse::{parser, prelude::*};
-    unimplemented!()
+
+    let parser = parser!("");
+
+    parser.parse(input).unwrap()
 }
 
 #[aoc(day1, part1)]
