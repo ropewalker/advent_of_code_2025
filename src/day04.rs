24b8015b0d4ef0061765c82d5488c08c1485eeab
Dima Lazarev <dmitry.lazarev@gmail.com>
2025-12-04 08:38:21 +0200

    Update Day 4

--- src/day04.rs
+++ src/day04.rs
@@ -1,5 +1,5 @@
 use aoc_runner_derive::{aoc, aoc_generator};
-use std::collections::HashSet;
+use std::collections::VecDeque;
 
 #[derive(Copy, Clone, Eq, PartialEq)]
 enum Tile {
@@ -17,59 +17,97 @@ fn parse_input(input: &str) -> Vec<Vec<Tile>> {
     parser.parse(input).unwrap()
 }
 
-fn accessible_rolls(grid: &[Vec<Tile>]) -> HashSet<(i32, i32)> {
+fn count_neighbours(grid: &[Vec<Tile>]) -> Vec<Vec<Option<usize>>> {
     use Tile::*;
 
     let max_x = grid[0].len() as i32;
     let max_y = grid.len() as i32;
 
-    let mut accessible_rolls = HashSet::new();
+    grid.iter()
+        .enumerate()
+        .map(|(y, row)| {
+            row.iter()
+                .enumerate()
+                .map(move |(x, tile)| {
+                    if *tile == RollOfPaper {
+                        let mut adjacent_rolls = 0;
 
-    for y in 0..max_y {
-        for x in 0..max_x {
-            if grid[y as usize][x as usize] == RollOfPaper {
-                let mut adjacent_rolls = 0;
-
-                for adjacent_y in i32::max(0, y - 1)..=i32::min(max_y - 1, y + 1) {
-                    for adjacent_x in i32::max(0, x - 1)..=i32::min(max_x - 1, x + 1) {
-                        if (adjacent_x != x || adjacent_y != y)
-                            && grid[adjacent_y as usize][adjacent_x as usize] == RollOfPaper
+                        for adjacent_y in
+                            i32::max(0, y as i32 - 1)..=i32::min(max_y - 1, y as i32 + 1)
                         {
-                            adjacent_rolls += 1;
+                            for adjacent_x in
+                                i32::max(0, x as i32 - 1)..=i32::min(max_x - 1, x as i32 + 1)
+                            {
+                                if (adjacent_x != x as i32 || adjacent_y != y as i32)
+                                    && grid[adjacent_y as usize][adjacent_x as usize] == RollOfPaper
+                                {
+                                    adjacent_rolls += 1;
+                                }
+                            }
                         }
+
+                        Some(adjacent_rolls)
+                    } else {
+                        None
                     }
-                }
-
-                if adjacent_rolls < 4 {
-                    accessible_rolls.insert((x, y));
-                }
-            }
-        }
-    }
-
-    accessible_rolls
+                })
+                .collect()
+        })
+        .collect()
 }
 
 #[aoc(day4, part1)]
 fn part1(grid: &[Vec<Tile>]) -> usize {
-    accessible_rolls(grid).len()
+    count_neighbours(grid)
+        .iter()
+        .flatten()
+        .filter(|tile| tile.is_some() && tile.unwrap() < 4)
+        .count()
 }
 
 #[aoc(day4, part2)]
 fn part2(grid: &[Vec<Tile>]) -> usize {
-    use Tile::*;
+    let max_x = grid[0].len() as i32;
+    let max_y = grid.len() as i32;
 
-    let mut grid = grid.to_owned();
-    let mut accessible_rolls = accessible_rolls(&grid);
-    let mut removed = 0;
+    let mut neighbours_grid = count_neighbours(grid);
+    let mut queue: VecDeque<(i32, i32)> = neighbours_grid
+        .iter_mut()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter_mut().enumerate().filter_map(move |(x, tile)| {
+                if tile.is_some() && tile.unwrap() < 4 {
+                    *tile = None;
+                    Some((x as i32, y as i32))
+                } else {
+                    None
+                }
+            })
+        })
+        .collect();
 
-    while !accessible_rolls.is_empty() {
-        accessible_rolls
-            .iter()
-            .for_each(|(x, y)| grid[*y as usize][*x as usize] = Empty);
+    let mut removed = queue.len();
 
-        removed += accessible_rolls.len();
-        accessible_rolls = crate::day04::accessible_rolls(&grid);
+    while let Some((x, y)) = queue.pop_front() {
+        for adjacent_y in i32::max(0, y - 1)..=i32::min(max_y - 1, y + 1) {
+            for adjacent_x in i32::max(0, x - 1)..=i32::min(max_x - 1, x + 1) {
+                if (adjacent_x != x || adjacent_y != y)
+                    && let Some(neighbour_count) =
+                        neighbours_grid[adjacent_y as usize][adjacent_x as usize]
+                {
+                    let new_neighbour_count = neighbour_count - 1;
+
+                    if new_neighbour_count < 4 {
+                        queue.push_back((adjacent_x, adjacent_y));
+                        neighbours_grid[adjacent_y as usize][adjacent_x as usize] = None;
+                        removed += 1;
+                    } else {
+                        neighbours_grid[adjacent_y as usize][adjacent_x as usize] =
+                            Some(new_neighbour_count);
+                    }
+                }
+            }
+        }
     }
 
     removed
