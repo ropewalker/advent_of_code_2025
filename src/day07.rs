ab6e92d28d4f71ea6f6a4bd6d8c6b25c9f341771
Dima Lazarev <dmitry.lazarev@gmail.com>
2025-12-07 10:10:26 +0200

    Update Day 7

--- src/day07.rs
+++ src/day07.rs
@@ -1,90 +1,43 @@
-use aoc_runner_derive::{aoc, aoc_generator};
-use std::collections::{HashMap, HashSet, VecDeque};
-
-#[aoc_generator(day7)]
-fn parse_input(input: &str) -> ((i32, i32), HashSet<(i32, i32)>) {
-    let mut starting_point = (0, 0);
-    let mut splitters = HashSet::new();
-
-    input.lines().enumerate().for_each(|(y, line)| {
-        line.chars()
-            .enumerate()
-            .for_each(|(x, character)| match character {
-                'S' => {
-                    starting_point = (x as i32, y as i32);
-                }
-                '^' => {
-                    splitters.insert((x as i32, y as i32));
-                }
-                _ => {}
-            })
-    });
-
-    (starting_point, splitters)
-}
+use aoc_runner_derive::aoc;
 
 #[aoc(day7, part1)]
-fn part1((staring_point, splitters): &((i32, i32), HashSet<(i32, i32)>)) -> usize {
-    let lowest_ordinate = *splitters.iter().map(|(_, y)| y).max().unwrap();
+fn part1(input: &str) -> usize {
+    let mut row = vec![false; input.lines().next().unwrap().len()];
+    let mut split_count = 0;
 
-    let mut queue = VecDeque::from([*staring_point]);
-    let mut visited_splitters = HashSet::new();
+    for line in input.lines().take(input.lines().count() - 1) {
+        for (index, character) in line.chars().enumerate() {
+            if character == '^' {
+                if row[index] {
+                    split_count += 1;
+                }
 
-    while let Some(point) = queue.pop_front() {
-        let next_point = (point.0, point.1 + 1);
-
-        if next_point.1 > lowest_ordinate {
-            break;
-        }
-
-        if splitters.contains(&next_point) {
-            if !visited_splitters.contains(&next_point) {
-                queue.push_back((next_point.0 - 1, next_point.1));
-                queue.push_back((next_point.0 + 1, next_point.1));
-
-                visited_splitters.insert(next_point);
+                row[index] = false;
+                row[index - 1] = true;
+                row[index + 1] = true;
+            } else if character == 'S' {
+                row[index] = true;
             }
-        } else {
-            queue.push_back(next_point);
         }
     }
 
-    visited_splitters.len()
+    split_count
 }
 
 #[aoc(day7, part2)]
-fn part2((staring_point, splitters): &((i32, i32), HashSet<(i32, i32)>)) -> usize {
-    let lowest_ordinate = *splitters.iter().map(|(_, y)| y).max().unwrap();
+fn part2(input: &str) -> usize {
+    let first_line = input.lines().next().unwrap();
+    let mut row = vec![1; first_line.len()];
 
-    let mut queue = VecDeque::from([(*staring_point, *staring_point)]);
-    let mut visited_splitters = HashMap::from([(*staring_point, 1)]);
-
-    let mut timeline_count = 0;
-
-    while let Some((point, prev_splitter)) = queue.pop_front() {
-        let prev_timeline_count = visited_splitters.get(&prev_splitter).unwrap();
-        let next_point = (point.0, point.1 + 1);
-
-        if next_point.1 > lowest_ordinate {
-            timeline_count += prev_timeline_count;
-            continue;
-        }
-
-        if splitters.contains(&next_point) {
-            if let Some(timeline_count) = visited_splitters.get(&next_point) {
-                visited_splitters.insert(next_point, timeline_count + prev_timeline_count);
-            } else {
-                queue.push_back(((next_point.0 - 1, next_point.1), next_point));
-                queue.push_back(((next_point.0 + 1, next_point.1), next_point));
-
-                visited_splitters.insert(next_point, *prev_timeline_count);
+    for line in input.lines().rev() {
+        for (index, character) in line.chars().enumerate() {
+            if character == '^' {
+                row[index] = row[index - 1] + row[index + 1];
             }
-        } else {
-            queue.push_back((next_point, prev_splitter));
         }
     }
 
-    timeline_count
+    row[first_line.find('S').unwrap()]
 }
 
 #[cfg(test)]
@@ -110,11 +63,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(part1(&parse_input(TEST_INPUT)), 21);
+        assert_eq!(part1(TEST_INPUT), 21);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(part2(&parse_input(TEST_INPUT)), 40);
+        assert_eq!(part2(TEST_INPUT), 40);
     }
 }
